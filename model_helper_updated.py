b309eaa4a5fa1d4a58135ffb527dee77e334264c
Kaya Güvendi <kayaguvendi@gmail.com>
2023-12-13 22:34:20 -0800

    Update model_helper_updated.py

--- model_helper_updated.py
+++ model_helper_updated.py
@@ -24,6 +24,7 @@ Note
         https://docs.dgl.ai/en/0.8.x/tutorials/models/1_gnn/9_gat.html
 '''
 class GATv2(nn.Module):
+    # Initialize GATv2 using the given hyperparameters, see class header for their values
     def __init__(
         self,
         num_layers,
@@ -89,6 +90,7 @@ class GATv2(nn.Module):
             )
         )
 
+    # Performs a forward pass through the layers of this GAT
     def forward(self, g, inputs):
         h = inputs
         for l in range(self.num_layers):
@@ -112,8 +114,8 @@ Note
     - But this implementation doesn't have a way to do input or attention drop, so we switched over the GATv2Conv
 
 '''
-
 class GATLayer(nn.Module):
+    # Initializes the GAT layer with the given parameters, which correspond to the input and output dimensions
     def __init__(self, in_dim, out_dim):
         super(GATLayer, self).__init__()
         # self.g = g
@@ -123,22 +125,26 @@ class GATLayer(nn.Module):
         self.attn_fc = nn.Linear(2 * out_dim, 1, bias=False)
         self.reset_parameters()
 
+    # Resets the parameters of the GAT using Xavier Normal initializations
     def reset_parameters(self):
         """Reinitialize learnable parameters."""
         gain = nn.init.calculate_gain('relu')
         nn.init.xavier_normal_(self.fc.weight, gain=gain)
         nn.init.xavier_normal_(self.attn_fc.weight, gain=gain)
 
+    # Computes attention values for the given edges
     def edge_attention(self, edges):
         # edge UDF for equation (2)
         z2 = torch.cat([edges.src['z'], edges.dst['z']], dim=1)
         a = self.attn_fc(z2)
         return {'e': F.leaky_relu(a)}
 
+    # Formats the messages created given the edges 
     def message_func(self, edges):
         # message UDF for equation (3) & (4)
         return {'z': edges.src['z'], 'e': edges.data['e']}
 
+    # Performs the aggregation for the GAT layer
     def reduce_func(self, nodes):
         # reduce UDF for equation (3) & (4)
         # equation (3)
@@ -147,6 +153,7 @@ class GATLayer(nn.Module):
         h = torch.sum(alpha * nodes.mailbox['z'], dim=1)
         return {'h': h}
 
+    # Performs a forward pass through the GAT layer given a graph g and node embeddings h
     def forward(self, g, h):
         # equation (1)
         z = self.fc(h)
@@ -167,6 +174,8 @@ Note
     - But this implementation doesn't have a way to do input or attention drop, so we switched over the GATv2Conv
 '''   
 class MultiHeadGATLayer(nn.Module):
+    # Initializes a multi-headed GAT layer operating with the number of given heads, input dimensions, and outputs dimension
+    # Defaults to concatenation of different heads' outputs, but can be made to mean them instead
     def __init__(self, in_dim, out_dim, num_heads, merge='cat'):
         super(MultiHeadGATLayer, self).__init__()
         self.heads = nn.ModuleList()
@@ -174,6 +183,7 @@ class MultiHeadGATLayer(nn.Module):
             self.heads.append(GATLayer(in_dim, out_dim))
         self.merge = merge
 
+    # Performs a forward pass through this GAT layer
     def forward(self, g, h):
         head_outs = [attn_head(g, h) for attn_head in self.heads]
         if self.merge == 'cat':
@@ -194,6 +204,9 @@ Note
 '''
 
 class GAT(nn.Module):
+    # Initializes a 3-layer multi-headed GAT with the given number of heads on the given device.
+    # Takes in inputs of in_dim dimensions and return out_dim-dimensional outputs while the intermediate
+    # values are kept at hidden_dim dimensions.
     def __init__(self, in_dim, hidden_dim, out_dim, num_heads, device='cuda'):
         super(GAT, self).__init__()
         self.layer1 = MultiHeadGATLayer(in_dim, hidden_dim, num_heads) # Input layer
@@ -201,6 +214,7 @@ class GAT(nn.Module):
         self.layer3 = MultiHeadGATLayer(hidden_dim * num_heads, out_dim, 1) # Output layer
         self.device = device
 
+    # Performs a forward pass through this GAT
     def forward(self, g, h):
         g = g.to(self.device)
         h = self.layer1(g, h)
